805efe3711883e198f8047d47fd7155dc24003b7
Josh Parnham <joshparnham@gmail.com>
2018-03-25 18:06:56 +1100

    Cleanup in Thumbnail file

--- QLScpt/GenerateThumbnailForURL.m
+++ QLScpt/GenerateThumbnailForURL.m
@@ -1,23 +1,9 @@
-#include <CoreFoundation/CoreFoundation.h>
-#include <CoreServices/CoreServices.h>
-#include <QuickLook/QuickLook.h>
+@import QuickLook;
 
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize);
-void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail);
-
-/* -----------------------------------------------------------------------------
-    Generate a thumbnail for file
-
-   This function's job is to create thumbnail for designated file as fast as possible
-   ----------------------------------------------------------------------------- */
-
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
-{
-    // To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
+OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize) {
     return noErr;
 }
 
-void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail)
-{
+void CancelThumbnailGeneration(void *thisInterface, QLThumbnailRequestRef thumbnail) {
     // Implement only if supported
 }
